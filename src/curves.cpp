e3e94f2e314ccf811d06b2b1feb2f6749ea614df
lrogerorrit <49691021+lrogerorrit@users.noreply.github.com>
2022-05-19 16:15:07 +0200

    added to render beizer

--- src/curves.cpp
+++ src/curves.cpp
@@ -1,10 +1,15 @@
 #include "curves.h"
 #include <assert.h>
+#include <iostream>
 
-BeizerCurve::BeizerCurve(std::vector<Vector3>& points, int numPoints)
+
+BeizerCurve::BeizerCurve(std::vector<Vector3>& points, float increments, bool cacheAtStart)
 {
 	this->curvePoints = points;
 	this->numPoints = points.size()-1;
+	//calculate cached segments
+	if (cacheAtStart)
+		this->cacheSegments(increments);
 }
 
 void BeizerCurve::addPoint(Vector3 pos, int index)
@@ -13,6 +18,7 @@ void BeizerCurve::addPoint(Vector3 pos, int index)
 		this->curvePoints.push_back(pos);
 	else 
 		this->curvePoints.insert(this->curvePoints.begin() + index, pos);
+	this->numPoints++;
 }
 
 Vector3 BeizerCurve::getPosition(float mu) //Extracted from http://paulbourke.net/geometry/bezier/
@@ -26,6 +32,7 @@ Vector3 BeizerCurve::getPosition(float mu) //Extracted from http://paulbourke.ne
 	munk = pow(1 - mu, (double)this->numPoints);
 	
 	for (int k = 0; k <= this->numPoints; ++k) {
+		
 		nn = this->numPoints;
 		kn = k;
 		nkn = (nn - k);
@@ -51,3 +58,15 @@ Vector3 BeizerCurve::getPosition(float mu) //Extracted from http://paulbourke.ne
 	return position;
 	
 }
+
+void BeizerCurve::cacheSegments(float increments)
+{
+	this->numSegments = ceil(1.0 / increments);
+	
+	this->cachedSegments.reserve(this->numSegments);
+	for (float i = 0.0f; i <= 1.0f; i += increments)
+		this->cachedSegments.push_back(this->getPosition(i));
+	for (int i = 0; i < this->cachedSegments.size() - 1; i++)
+		this->segmentDistances.push_back(this->cachedSegments[i].distance(this->cachedSegments[i + 1]));
+}
+
--- src/curves.h
+++ src/curves.h
@@ -6,16 +6,50 @@ class BeizerCurve
 {
 	public:
 		std::vector<Vector3> curvePoints;
+		std::vector<Vector3> cachedSegments;
+		std::vector<float> segmentDistances;
 		int numPoints=0;
+		int numSegments = 0;
+		float speedConstant = 1;
 		
 		
-		BeizerCurve(std::vector<Vector3>& points, int numPoints); //There should be be n+1 points (last point isnt computed though)
+		BeizerCurve(std::vector<Vector3>& points, float increments=.05,bool cacheAtStart=true); //There should be be n+1 points (last point isnt computed though)
 	
 		void addPoint(Vector3 pos, int index = -1);
 		inline Vector3 getPoint(int index) { return curvePoints[index]; };
+		inline std::vector<Vector3> getPoints() { return curvePoints; };
 		
 		Vector3 getPosition(float mu);
-	
+		
+		void clearSegmentCache() {
+			cachedSegments.clear();
+			segmentDistances.clear();
+		}
+		void cacheSegments(float increments=.05);
+		
+		
+		inline Vector3 getSegment(int i) {
+			return cachedSegments[i];
+		}
+		inline std::vector<Vector3>& getSegments() {
+			return cachedSegments;
+		}
+
+		inline Vector3 getSegmentDirection(int i) {
+			
+			float segmentSize= 1.0f / (float)numSegments;
+			return (getPosition(i*segmentSize) - getPosition((i*segmentSize) + .0001)).normalize();
+		}
+
+		inline float getSegmentDistance(int i) {
+			return segmentDistances[i];
+		}
+		
+		inline int getSegmentFromMu(float mu) {
+			float segmentSize= 1.0f / (float)numSegments;
+			int segment = (int)floor(mu / segmentSize);
+			return segment;
+		}
 	
 };
 
