84f81ae22b24c1b5a85c407692df6f6380f3d187
lrogerorrit <49691021+lrogerorrit@users.noreply.github.com>
2022-05-27 10:19:17 +0200

    normalised movement

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,6 +8,7 @@
 const float maxSpeed=10;
 const float y_sensitivity = 15;
 const float x_sensitivity = 10;
+const float decel_threshold = .98;
 
 
 
@@ -18,7 +19,7 @@ Player::Player()
 	texture->load("data/playerTemp.png");
 	Shader* shader = Shader::Get("data/shaders/basic.vs", "data/shaders/texture.fs");
 	playerMesh = new MeshEntity(mesh, texture, shader);
-	ropeLengthRadius = 30.0f;
+	ropeLengthRadius = 100.0f;
 	this->trainHandler = TrainHandler::instance;
 	
 }
@@ -41,7 +42,7 @@ float Player::getDecelerationMultiplier(float distanceFromCar, bool movingAway) 
 	if (movingAway) {
 		if (distanceFromCar >= this->ropeLengthRadius)
 			return 100.0f;
-		else if (distanceFromCar > this->ropeLengthRadius * .9)
+		else if (distanceFromCar > this->ropeLengthRadius * decel_threshold)
 			return 3.0f;
 	}
 	return .8f;
@@ -94,11 +95,11 @@ void Player::updatePlayer(double seconds_elapsed)
 	
 	if (Input::isKeyPressed(SDL_SCANCODE_W) || Input::isKeyPressed(SDL_SCANCODE_UP)) {
 		wasMoved = true;
-		speedVector+= front*seconds_elapsed*acceleration;
+		speedVector+= front*(acceleration* seconds_elapsed);
 	}
 	if (Input::isKeyPressed(SDL_SCANCODE_S) || Input::isKeyPressed(SDL_SCANCODE_DOWN)) {
 		wasMoved = true;
-		speedVector+= front*seconds_elapsed* (-acceleration);
+		speedVector+= front* (-acceleration* seconds_elapsed);
 	}
 	if (Input::isKeyPressed(SDL_SCANCODE_A) || Input::isKeyPressed(SDL_SCANCODE_LEFT)) {
 		wasMoved = true;
@@ -110,7 +111,7 @@ void Player::updatePlayer(double seconds_elapsed)
 	}
 
 	
-	this->playerMesh->model.translateGlobal(speedVector.x, speedVector.y, speedVector.z);
+	this->playerMesh->model.translateGlobal(speedVector.x* seconds_elapsed, speedVector.y* seconds_elapsed, speedVector.z* seconds_elapsed);
 
 	Vector3 newPos= playerMesh->getPosition();
 	//print speedvector to console
@@ -118,10 +119,10 @@ void Player::updatePlayer(double seconds_elapsed)
 	
 	float distanceFromCar = this->playerMesh->getGlobalMatrix().getTranslation().distance(carPos); //For now its from 0,0,0
 	bool movingAway = (oldPos.distance(carPos) <= newPos.distance(carPos));  //TODO: Change vec3(0,0,0) to car position;
-	if (!wasMoved||distanceFromCar> ropeLengthRadius*.9&&movingAway) {
+	if (!wasMoved||(distanceFromCar> ropeLengthRadius* decel_threshold &&movingAway)||distanceFromCar>ropeLengthRadius) {
 		float multiplier = getDecelerationMultiplier(distanceFromCar, movingAway); //((distanceFromCar > ropeLengthRadius) &&movingAway) ? 3.0 : .8;
-		std::cout <<"distance from car: "<<distanceFromCar << " - multiplier: " << multiplier << std::endl;
-		if (distanceFromCar > ropeLengthRadius) {
+		//std::cout <<"m a "<<movingAway<<" car dist: "<<distanceFromCar << "mult: " << multiplier << std::endl;
+		if (distanceFromCar > ropeLengthRadius||multiplier==100.0f) {
 			if(movingAway)
 				speedVector = Vector3(0, 0, 0);  
 			Vector3 carDisplacement = trainHandler->getCarDisplacement(0);
@@ -129,11 +130,13 @@ void Player::updatePlayer(double seconds_elapsed)
 		}
 		else {
 			speedVector += speedVector * (-multiplier) * seconds_elapsed;
-			if (abs(speedVector.length()) <= .0002)
+			if (abs(speedVector.length()) <= .002)
 				speedVector = Vector3(0, 0, 0);
 		}
 	}
-
+	/*else
+		std::cout <<"m a "<<movingAway<<" car dist: "<<distanceFromCar << "mult: N/A"<< std::endl;
+		*/
 	Vector3 newEye = this->playerMesh->getGlobalMatrix().getTranslation();
 		
 	cam->lookAt(newEye, newEye + front , top);
