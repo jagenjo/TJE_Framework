6b703a3e8b491b88d1f518beba2aeffafc09d340
OriolGallego2001 <oriol.gallego01@estudiant.upf.edu>
2022-06-02 21:28:45 +0200

    DepoStage creation v0

---
+++ src/stages/DepositionStage.cpp
@@ -0,0 +1,17 @@
+#include "../Scene.h"
+#include "DepositionStage.h"
+
+
+
+
+void DepositionStage::initStage() {
+
+}
+
+void DepositionStage::update(double seconds_elapsed) {
+
+}
+
+void DepositionStage::render() {
+
+}
---
+++ src/stages/DepositionStage.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../framework.h"
+#include "Stage.h"
+#include "../Player.h"
+
+
+enum class eBlockType {
+	Wall1,
+	Wall2,
+	Floor1,
+	Floor2,
+	Ramp
+};
+
+class DepositionStage :
+	public Stage
+{	
+	private:
+		void loadAssets();
+		
+	public:
+
+
+		void initStage();
+
+		void update(double deltaTime);
+		void render();
+
+};
+
